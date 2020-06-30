7e7244fa65e79bb195e776d51528f16d838e2589
winarto007 <winarto007zing@gmail.com>
2020-06-30 11:52:56 +0700

    fix login session

--- application/controllers/Guest.php
+++ application/controllers/Guest.php
@@ -14,12 +14,39 @@ class Guest extends CI_Controller {
         } else {
             $username=htmlspecialchars($this->input->post('username',TRUE),ENT_QUOTES);
             $password=htmlspecialchars($this->input->post('password',TRUE),ENT_QUOTES);
+     
+            $cek_kaprodi=$this->model_auth->auth_kaprodi($username,$password);
+            $cek_baak=$this->model_auth->auth_baak($username,$password);
+            $cek_mahasiswa=$this->model_auth->auth_mahasiswa($username,$password);
 
-            $cek_kaprodi =$this->model_auth->auth_kaprodi($username, $password);
-            $cek_baak =$this->model_auth->auth_baak($username, $password);
-            $cek_mahasiswa =$this->model_auth->auth_mahasiswa($username, $password);
-            
-            if ($cek_kaprodi && $cek_baak && $cek_mahasiswa == FALSE) {
+            if ($cek_kaprodi->num_rows() > 0){
+                $data=$cek_kaprodi->row_array();
+                if ($data['role_id'] == '1') {
+                    $this->session->set_userdata('masuk',TRUE);
+                    $this->session->set_userdata('akses','1');
+                    $this->session->set_userdata('kode',$data['kode']);
+                    $this->session->set_userdata('nama',$data['nama']);
+                    redirect(base_url('Kaprodi/Dashboard'));
+                }               
+            }else if ($cek_baak->num_rows() > 0) {
+                $data=$cek_baak->row_array();
+                if ($data['rolde_id'] == '2') {
+                    $this->session->set_userdata('masuk',TRUE);
+                    $this->session->set_userdata('akses','2');
+                    $this->session->set_userdata('kode',$data['kode']);
+                    $this->session->set_userdata('nama',$data['nama']);
+                    redirect(base_url('BAAK/Dashboard'));
+                }
+            } else if($cek_mahasiswa->num_rows() > 0){
+                $data=$cek_mahasiswa->row_array();
+                if ($data['role_id'] == '3') {
+                    $this->session->set_userdata('masuk',TRUE);
+                    $this->session->set_userdata('akses','3');
+                    $this->session->set_userdata('npm',$data['npm']);
+                    $this->session->set_userdata('nama_mhs',$data['nama_mhs']);
+                    redirect(base_url('Mahasiswa/Dashboard'));
+                }
+            } else {
                 $this->session->set_flashdata('pesan','<div class="alert alert-danger alert-dismissible fade show" role="alert">
                                                         <b>Username atau Password Salah.</b>
                                                         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -27,40 +54,7 @@ class Guest extends CI_Controller {
                                                         </button>
                                                     </div>');
                 $this->login();
-            } else {
-                if ($cek_kaprodi->num_rows() > 0) {
-                    $data = $cek_kaprodi->row_array();
-                    $this->session->set_userdata('masuk',TRUE);
-                    if ($data['role_id'] == 1) {
-                        $this->session->set_userdata('kode', $data['kode']);
-                        $this->session->set_userdata('nama', $data['nama']);
-                        $this->session->set_userdata('email', $data['email']);
-                        $this->session->set_userdata('password', $data['password']);
-                        redirect(base_url('Kaprodi/Dashboard'));
-                    }
-                } else if ($cek_baak->num_rows() > 0) {
-                    $data = $cek_baak->row_array();
-                    $this->session->set_userdata('masuk',TRUE);
-                    if ($data['role_id'] == 2) {
-                        $this->session->set_userdata('kode', $data['kode']);
-                        $this->session->set_userdata('nama', $data['nama']);
-                        $this->session->set_userdata('email', $data['email']);
-                        $this->session->set_userdata('password', $data['password']);
-                        redirect(base_url('BAAK/Dashboard'));
-                    }
-                } else if ($cek_mahasiswa->num_rows() > 0) {
-                    $data = $cek_mahasiswa->row_array();
-                    $this->session->set_userdata('masuk',TRUE);
-                    if ($data['role_id'] == 3) {
-                        $this->session->set_userdata('npm', $data['npm']);
-                        $this->session->set_userdata('nama_mhs', $data['nama_mhs']);
-                        $this->session->set_userdata('email', $data['email']);
-                        $this->session->set_userdata('semester', $data['semester']);
-                        $this->session->set_userdata('password', $data['password']);
-                        redirect(base_url('Mahasiswa/Dashboard'));
-                    }
-                }
-            } 
+            }
         }
     }
 
--- application/models/model_auth.php
+++ application/models/model_auth.php
@@ -1,16 +1,17 @@
 <?php
 class Model_Auth extends CI_Model {
-    public function auth_kaprodi($username,$password){
+    function auth_kaprodi($username,$password){
         $query=$this->db->query("SELECT * FROM tbl_kaprodi WHERE kode='$username' AND pass=MD5('$password') LIMIT 1");
         return $query;
     }
-
-    public function auth_baak($username,$password){
+    
+    function auth_baak($username,$password){
         $query=$this->db->query("SELECT * FROM tbl_baak WHERE kode='$username' AND pass=MD5('$password') LIMIT 1");
         return $query;
     }
 
-    public function auth_mahasiswa($username,$password){
+    //cek nim dan password mahasiswa
+    function auth_mahasiswa($username,$password){
         $query=$this->db->query("SELECT * FROM tbl_mahasiswa WHERE npm='$username' AND pass=MD5('$password') LIMIT 1");
         return $query;
     }
