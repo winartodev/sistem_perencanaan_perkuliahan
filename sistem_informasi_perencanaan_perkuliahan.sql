ea670036bf4c5ee79cd93f718e9b2936aec05e2c
Winarto <winarto007zing@gmail.com>
2020-08-18 13:01:41 +0700

    Update sistem_informasi_perencanaan_perkuliahan.sql

--- sistem_informasi_perencanaan_perkuliahan.sql
+++ sistem_informasi_perencanaan_perkuliahan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 17, 2020 at 05:16 AM
+-- Generation Time: Aug 18, 2020 at 08:01 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 
@@ -70,7 +70,6 @@ INSERT INTO `tbl_dosen` (`kode_dosen`, `nama_dosen`, `email`, `no_telp`) VALUES
 ('007', 'Dyah Ayu Megawaty, S.Kom., M.Kom.', '', ''),
 ('008', 'Faruk Ulum, S.T., M.T.I.', '', ''),
 ('009', 'Heni Sulistiani, S.Kom., M.Kom.', '', ''),
-('010', 'Imam Ahmad, S.Kom., M.Kom.', '', ''),
 ('011', 'M. Najib Dwi Satria, S.Kom., M.T.', '', ''),
 ('012', 'Neneng, S.Kom., M.Kom.', '', ''),
 ('013', 'Novia Utami Putri, S.T.', '', ''),
@@ -98,7 +97,7 @@ INSERT INTO `tbl_dosen` (`kode_dosen`, `nama_dosen`, `email`, `no_telp`) VALUES
 ('035', 'Ardian Cahyadi, M.Pd.', '', ''),
 ('036', 'Dayu Rika, S.Pd., M.Pd.', '', ''),
 ('037', 'Agus Budianto, S.Kom.', '', ''),
-('038', 'rrr', '', '');
+('038', 'rrr1', '', '');
 
 -- --------------------------------------------------------
 
@@ -108,7 +107,7 @@ INSERT INTO `tbl_dosen` (`kode_dosen`, `nama_dosen`, `email`, `no_telp`) VALUES
 
 CREATE TABLE `tbl_jadwal` (
   `id_jdwl` int(11) NOT NULL,
-  `kode_kelas` int(10) NOT NULL,
+  `id_perencanaan` int(10) NOT NULL,
   `npm` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -116,14 +115,8 @@ CREATE TABLE `tbl_jadwal` (
 -- Dumping data for table `tbl_jadwal`
 --
 
-INSERT INTO `tbl_jadwal` (`id_jdwl`, `kode_kelas`, `npm`) VALUES
-(48, 29, '1811010008'),
-(49, 29, '1711010008'),
-(50, 29, '1611010008'),
-(51, 29, '1511010008'),
-(53, 36, '1811010008'),
-(54, 41, '1811010008'),
-(56, 33, '1811010008');
+INSERT INTO `tbl_jadwal` (`id_jdwl`, `id_perencanaan`, `npm`) VALUES
+(71, 8, '1811010008');
 
 -- --------------------------------------------------------
 
@@ -165,8 +158,7 @@ CREATE TABLE `tbl_keilmuan` (
 
 INSERT INTO `tbl_keilmuan` (`id_keilmuan`, `kode_dosen`, `bidang_ilmu`, `kode_mk`) VALUES
 (7, '001', 'Teknik Informatika', 'TIF001-19'),
-(8, '002', 'Teknik Informatika', 'TIF001-19'),
-(9, '003', 'Teknik Informatika', 'TIF003-19');
+(8, '002', 'Teknik Informatika', 'TIF001-19');
 
 -- --------------------------------------------------------
 
@@ -176,9 +168,6 @@ INSERT INTO `tbl_keilmuan` (`id_keilmuan`, `kode_dosen`, `bidang_ilmu`, `kode_mk
 
 CREATE TABLE `tbl_kelas` (
   `id` int(11) NOT NULL,
-  `kode_kelompok` int(3) NOT NULL,
-  `kode_mk` varchar(20) NOT NULL,
-  `kode_dosen` varchar(20) NOT NULL,
   `angkatan` varchar(3) NOT NULL,
   `nama_kelas` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -187,22 +176,19 @@ CREATE TABLE `tbl_kelas` (
 -- Dumping data for table `tbl_kelas`
 --
 
-INSERT INTO `tbl_kelas` (`id`, `kode_kelompok`, `kode_mk`, `kode_dosen`, `angkatan`, `nama_kelas`) VALUES
-(29, 1, 'TIF001-19', '001', '18', 'IF 18 A'),
-(30, 1, 'TIF001-19', '001', '18', 'IF 18 B'),
-(31, 1, 'TIF002-19', '001', '18', 'IF 18 A'),
-(32, 1, 'TIF002-19', '001', '18', 'IF 18 B'),
-(33, 1, 'TIF003-19', '002', '15', 'IF 15 A'),
-(34, 1, 'TIF003-19', '002', '15', 'IF 15 B'),
-(35, 1, 'TIF004-19', '002', '16', 'IF 16 A'),
-(36, 1, 'TIF004-19', '002', '16', 'IF 16 B'),
-(37, 1, 'TIF004-19', '002', '16', 'IF 16 C'),
-(38, 1, 'TIF005-19', '002', '18', 'IF 17 A'),
-(39, 1, 'TIF005-19', '002', '17', 'IF 17 B'),
-(40, 1, 'TIF006-19', '002', '16', 'IF 16 A'),
-(41, 1, 'TIF006-19', '002', '16', 'IF 16 B'),
-(42, 1, 'TIF006-19', '002', '16', 'IF 16 C'),
-(43, 1, 'TIF006-19', '002', '16', 'IF 16 D');
+INSERT INTO `tbl_kelas` (`id`, `angkatan`, `nama_kelas`) VALUES
+(2, '16', 'IF 16 A'),
+(3, '18', 'IF 18 A'),
+(4, '15', 'IF 15 A'),
+(5, '15', 'IF 15 B'),
+(6, '15', 'IF 15 C'),
+(7, '16', 'IF 16 B'),
+(8, '16', 'IF 16 C'),
+(9, '17', 'IF 17 A'),
+(10, '17', 'IF 17 B'),
+(11, '17', 'IF 17 C'),
+(12, '18', 'IF 18 B'),
+(13, '18', 'IF 18 C');
 
 -- --------------------------------------------------------
 
@@ -232,7 +218,7 @@ INSERT INTO `tbl_kelompok` (`kode_kelompok`, `nama_kelompok`) VALUES
 CREATE TABLE `tbl_kurikulum` (
   `id_kurikulum` int(11) NOT NULL,
   `id_tahun_akademik` int(11) NOT NULL,
-  `angkatan` varchar(3) NOT NULL,
+  `angkatan_kurikulum` varchar(3) NOT NULL,
   `semester_kurikulum` varchar(10) NOT NULL,
   `kode_mk` varchar(35) NOT NULL,
   `kode_dosen` varchar(35) NOT NULL
@@ -242,8 +228,9 @@ CREATE TABLE `tbl_kurikulum` (
 -- Dumping data for table `tbl_kurikulum`
 --
 
-INSERT INTO `tbl_kurikulum` (`id_kurikulum`, `id_tahun_akademik`, `angkatan`, `semester_kurikulum`, `kode_mk`, `kode_dosen`) VALUES
-(1, 6, '18', '2', 'TIF001-19', '002');
+INSERT INTO `tbl_kurikulum` (`id_kurikulum`, `id_tahun_akademik`, `angkatan_kurikulum`, `semester_kurikulum`, `kode_mk`, `kode_dosen`) VALUES
+(11, 6, '15', '1', 'TIF001-19', '001'),
+(12, 6, '15', '2', 'TIF001-19', '002');
 
 -- --------------------------------------------------------
 
@@ -291,7 +278,7 @@ CREATE TABLE `tbl_matakuliah` (
 --
 
 INSERT INTO `tbl_matakuliah` (`kode_mk`, `nama_mk`, `sks_teori`, `sks_praktek`, `total_sks`, `angkatan`) VALUES
-('TIF001-19', 'Dasar-Dasar Pemrograman (Gel. 1)', 0, 2, 2, '18'),
+('TIF001-19', 'Dasar-Dasar Pemrograman (Gel. 1)', 2, 2, 4, '18'),
 ('TIF002-19', 'Praktikum Pemrograman I (Gel. 1)', 0, 0, 1, '18'),
 ('TIF003-19', 'Proyek Perangkat Lunak', 0, 0, 3, '15'),
 ('TIF004-19', 'Pemrograman Mobile ', 0, 0, 3, '16'),
@@ -380,21 +367,22 @@ INSERT INTO `tbl_pengumuman` (`id`, `judul`, `tanggal`, `konten`) VALUES
 --
 
 CREATE TABLE `tbl_perencanaan` (
-  `id_tmp` int(11) NOT NULL,
-  `npm` varchar(20) NOT NULL,
-  `kode_kelas` int(3) NOT NULL,
-  `angkatan` varchar(3) NOT NULL,
-  `status` enum('Di proses') NOT NULL
+  `id_perencanaan` int(11) NOT NULL,
+  `kode_kelompok` int(3) NOT NULL,
+  `angkatan_perencanaan` varchar(3) NOT NULL,
+  `semester_perencanaan` varchar(2) NOT NULL,
+  `kode_mk` varchar(20) NOT NULL,
+  `kode_dosen` varchar(20) NOT NULL,
+  `kode_kelas` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbl_perencanaan`
 --
 
-INSERT INTO `tbl_perencanaan` (`id_tmp`, `npm`, `kode_kelas`, `angkatan`, `status`) VALUES
-(78, '1811010009', 40, '18', 'Di proses'),
-(81, '1811010008', 34, '18', 'Di proses'),
-(82, '1811010008', 38, '18', 'Di proses');
+INSERT INTO `tbl_perencanaan` (`id_perencanaan`, `kode_kelompok`, `angkatan_perencanaan`, `semester_perencanaan`, `kode_mk`, `kode_dosen`, `kode_kelas`) VALUES
+(8, 1, '15', '1', 'TIF001-19', '002', 6),
+(9, 1, '15', '2', 'TIF001-19', '002', 5);
 
 -- --------------------------------------------------------
 
@@ -414,6 +402,20 @@ CREATE TABLE `tbl_tahun_akademik` (
 
 INSERT INTO `tbl_tahun_akademik` (`id_ta`, `tahun_akademik`, `semester`) VALUES
 (6, 2019, 'Ganjil');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_verifikasi_perencanaan`
+--
+
+CREATE TABLE `tbl_verifikasi_perencanaan` (
+  `id_tmp` int(11) NOT NULL,
+  `npm` varchar(20) NOT NULL,
+  `id_perencanaan` int(3) NOT NULL,
+  `angkatan` varchar(3) NOT NULL,
+  `status` enum('Di proses') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
@@ -436,8 +438,8 @@ ALTER TABLE `tbl_dosen`
 --
 ALTER TABLE `tbl_jadwal`
   ADD PRIMARY KEY (`id_jdwl`),
-  ADD KEY `fk_kelas` (`kode_kelas`),
-  ADD KEY `fk_npm` (`npm`);
+  ADD KEY `fk_npm` (`npm`),
+  ADD KEY `fk_kelas` (`id_perencanaan`);
 
 --
 -- Indexes for table `tbl_kaprodi`
@@ -457,10 +459,7 @@ ALTER TABLE `tbl_keilmuan`
 -- Indexes for table `tbl_kelas`
 --
 ALTER TABLE `tbl_kelas`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_mk` (`kode_mk`),
-  ADD KEY `fk_dosen` (`kode_dosen`),
-  ADD KEY `fk_kelompok` (`kode_kelompok`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tbl_kelompok`
@@ -474,8 +473,8 @@ ALTER TABLE `tbl_kelompok`
 ALTER TABLE `tbl_kurikulum`
   ADD PRIMARY KEY (`id_kurikulum`),
   ADD KEY `fk_kurikulum_dosen` (`kode_dosen`),
-  ADD KEY `fk_kurikulum_mk` (`kode_mk`),
-  ADD KEY `fk_kurikulum_tahun_akademik` (`id_tahun_akademik`);
+  ADD KEY `fk_kurikulum_tahun_akademik` (`id_tahun_akademik`),
+  ADD KEY `fk_kurikulum_mk` (`kode_mk`);
 
 --
 -- Indexes for table `tbl_mahasiswa`
@@ -505,14 +504,24 @@ ALTER TABLE `tbl_pengumuman`
 -- Indexes for table `tbl_perencanaan`
 --
 ALTER TABLE `tbl_perencanaan`
-  ADD PRIMARY KEY (`id_tmp`),
-  ADD KEY `fk_kode_kelas` (`kode_kelas`);
+  ADD PRIMARY KEY (`id_perencanaan`),
+  ADD KEY `fk_perencanaan_dosen` (`kode_dosen`),
+  ADD KEY `fk_perencanaan_matakuliah` (`kode_mk`),
+  ADD KEY `fk_perencanaan_kelompok` (`kode_kelompok`),
+  ADD KEY `fk_perencanaan_kelas` (`kode_kelas`);
 
 --
 -- Indexes for table `tbl_tahun_akademik`
 --
 ALTER TABLE `tbl_tahun_akademik`
   ADD PRIMARY KEY (`id_ta`);
+
+--
+-- Indexes for table `tbl_verifikasi_perencanaan`
+--
+ALTER TABLE `tbl_verifikasi_perencanaan`
+  ADD PRIMARY KEY (`id_tmp`),
+  ADD KEY `fk_kode_kelas` (`id_perencanaan`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -522,7 +531,7 @@ ALTER TABLE `tbl_tahun_akademik`
 -- AUTO_INCREMENT for table `tbl_jadwal`
 --
 ALTER TABLE `tbl_jadwal`
-  MODIFY `id_jdwl` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+  MODIFY `id_jdwl` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
 
 --
 -- AUTO_INCREMENT for table `tbl_keilmuan`
@@ -534,7 +543,7 @@ ALTER TABLE `tbl_keilmuan`
 -- AUTO_INCREMENT for table `tbl_kelas`
 --
 ALTER TABLE `tbl_kelas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `tbl_kelompok`
@@ -546,7 +555,7 @@ ALTER TABLE `tbl_kelompok`
 -- AUTO_INCREMENT for table `tbl_kurikulum`
 --
 ALTER TABLE `tbl_kurikulum`
-  MODIFY `id_kurikulum` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_kurikulum` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `tbl_penginputan`
@@ -564,13 +573,19 @@ ALTER TABLE `tbl_pengumuman`
 -- AUTO_INCREMENT for table `tbl_perencanaan`
 --
 ALTER TABLE `tbl_perencanaan`
-  MODIFY `id_tmp` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
+  MODIFY `id_perencanaan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `tbl_tahun_akademik`
 --
 ALTER TABLE `tbl_tahun_akademik`
   MODIFY `id_ta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `tbl_verifikasi_perencanaan`
+--
+ALTER TABLE `tbl_verifikasi_perencanaan`
+  MODIFY `id_tmp` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=99;
 
 --
 -- Constraints for dumped tables
@@ -580,7 +595,7 @@ ALTER TABLE `tbl_tahun_akademik`
 -- Constraints for table `tbl_jadwal`
 --
 ALTER TABLE `tbl_jadwal`
-  ADD CONSTRAINT `fk_kelas` FOREIGN KEY (`kode_kelas`) REFERENCES `tbl_kelas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_kelas` FOREIGN KEY (`id_perencanaan`) REFERENCES `tbl_perencanaan` (`id_perencanaan`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_npm` FOREIGN KEY (`npm`) REFERENCES `tbl_mahasiswa` (`npm`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
@@ -589,14 +604,6 @@ ALTER TABLE `tbl_jadwal`
 ALTER TABLE `tbl_keilmuan`
   ADD CONSTRAINT `fk_keilmuan_dosen` FOREIGN KEY (`kode_mk`) REFERENCES `tbl_matakuliah` (`kode_mk`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_kode_dosen` FOREIGN KEY (`kode_dosen`) REFERENCES `tbl_dosen` (`kode_dosen`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `tbl_kelas`
---
-ALTER TABLE `tbl_kelas`
-  ADD CONSTRAINT `fk_dosen` FOREIGN KEY (`kode_dosen`) REFERENCES `tbl_dosen` (`kode_dosen`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_kelompok` FOREIGN KEY (`kode_kelompok`) REFERENCES `tbl_kelompok` (`kode_kelompok`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_mk` FOREIGN KEY (`kode_mk`) REFERENCES `tbl_matakuliah` (`kode_mk`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `tbl_kurikulum`
@@ -610,7 +617,16 @@ ALTER TABLE `tbl_kurikulum`
 -- Constraints for table `tbl_perencanaan`
 --
 ALTER TABLE `tbl_perencanaan`
-  ADD CONSTRAINT `fk_kode_kelas` FOREIGN KEY (`kode_kelas`) REFERENCES `tbl_kelas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_perencanaan_dosen` FOREIGN KEY (`kode_dosen`) REFERENCES `tbl_dosen` (`kode_dosen`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_perencanaan_kelas` FOREIGN KEY (`kode_kelas`) REFERENCES `tbl_kelas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_perencanaan_kelompok` FOREIGN KEY (`kode_kelompok`) REFERENCES `tbl_kelompok` (`kode_kelompok`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_perencanaan_matakuliah` FOREIGN KEY (`kode_mk`) REFERENCES `tbl_matakuliah` (`kode_mk`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tbl_verifikasi_perencanaan`
+--
+ALTER TABLE `tbl_verifikasi_perencanaan`
+  ADD CONSTRAINT `fk_kode_kelas` FOREIGN KEY (`id_perencanaan`) REFERENCES `tbl_perencanaan` (`id_perencanaan`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
