1b220b93595f97a1685fc7befbb8f8bd7499a269
Winarto <winarto007zing@gmail.com>
2020-08-19 10:06:28 +0700

    Create Perencanaan in BAAK

---
+++ application/controllers/BAAK/Perencanaan.php
@@ -0,0 +1,233 @@
+<?php
+class Perencanaan extends CI_Controller {
+    public function __construct() {
+        parent::__construct();
+
+        if ($this->session->userdata('role_id') != '2') {
+            $this->session->set_flashdata('pesan','<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                                    <b>Anda Belum Login.</b>
+                                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>');
+            redirect(base_url('guest/login'));
+        }
+    }
+    
+    public function index() {
+        $data['perencanaan'] = $this->model_perencanaan->read_data();
+        $this->load->view('templates/baak/header');
+        $this->load->view('templates/baak/sidebar');
+        $this->load->view('baak/perencanaan', $data);
+        $this->load->view('templates/baak/footer');
+    }
+
+    public function add() {
+        $data['kelompok']   = $this->model_kelas->get_nama_Kelompok();
+        // $data['kelas']   = $this->model_kelas->read_data();
+        // $data['matakuliah'] = $this->model_matakuliah->read_data()->result();
+        // $data['dosen']      = $this->model_dosen->read_data()->result();
+        $this->load->view('templates/baak/header');
+        $this->load->view('templates/baak/sidebar');
+        $this->load->view('baak/form_insert_perencanaan', $data);
+        $this->load->view('templates/baak/footer');
+    }
+
+    public function edit($id) {
+        $where = array('id_perencanaan' => $id);
+        $data['perencanaan']    = $this->model_perencanaan->form_edit($where, 'tbl_perencanaan');
+        $data['kelompok']       = $this->model_perencanaan->get_nama_Kelompok();
+        $data['matakuliah']     = $this->model_matakuliah->read_data()->result();
+        $data['kelas']          = $this->model_kelas->read_data();
+        $data['dosen']          = $this->model_dosen->read_data()->result();
+        $this->load->view('templates/baak/header');
+        $this->load->view('templates/baak/sidebar');
+        $this->load->view('baak/form_edit_perencanaan', $data);
+        $this->load->view('templates/baak/footer');
+    }
+
+    public function info($id) {
+        $where = array('id_perencanaan' => $id);
+        $data['perencanaan']    = $this->model_perencanaan->form_edit($where, 'tbl_perencanaan');
+        $data['mahasiswa']      = $this->model_perencanaan->get_mahasiswa($id)->result();
+        $data['A15']            = $this->model_perencanaan->count_angkatan($id,'15','tbl_jadwal');
+        $data['A16']            = $this->model_perencanaan->count_angkatan($id,'16','tbl_jadwal');
+        $data['A17']            = $this->model_perencanaan->count_angkatan($id,'17','tbl_jadwal');
+        $data['A18']            = $this->model_perencanaan->count_angkatan($id,'18','tbl_jadwal');
+        $this->load->view('templates/baak/header');
+        $this->load->view('templates/baak/sidebar');
+        $this->load->view('baak/info_perencanaan', $data);
+        $this->load->view('templates/baak/footer');
+    }
+
+    public function insert_perencanaan() {
+        $this->_rules();
+             
+        if ($this->form_validation->run() == FALSE) {
+            $this->add();
+        } else {
+            $kode_kelompok      = $this->input->post('kode_kelompok');
+            $angkatan           = $this->input->post('angkatan');
+            $semester           = $this->input->post('semester');
+            $kode_mk            = $this->input->post('kode_mk');
+            $kode_dosen         = $this->input->post('kode_dosen');
+            $kode_kelas         = $this->input->post('kode_kelas');
+
+            $data = array (
+                'kode_kelompok'         => $kode_kelompok,
+                'angkatan_perencanaan'              => $angkatan,
+                'semester_perencanaan'  => $semester,
+                'kode_mk'               => $kode_mk,
+                'kode_dosen'            => $kode_dosen,
+                'kode_kelas'            => $kode_kelas,
+                'status_perencanaan'      => 'belum_verifikasi'
+            );
+
+            $this->model_perencanaan->insert_data($data, 'tbl_perencanaan');
+            $this->session->set_flashdata('pesan','<div class="alert alert-info alert-dismissible fade show" role="alert">
+                                                        <b>Perencanaan Berhasil di Buat.</b>
+                                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                        </button>
+                                                    </div>');
+            redirect(base_url('baak/perencanaan')); 
+        }
+    }
+
+    public function filter_matakuliah () 
+    {
+        $angkatan = $this->input->post('angkatan');
+        $semester = $this->input->post('semester');
+
+        $data = $this->model_perencanaan->filter_matakuliah($angkatan, $semester);
+
+        if ($angkatan || $semester) 
+        {
+            ?> 
+                <option value="">Pilih Matakuliah</option>
+            <?php
+            foreach($data as $row): ?>
+                <option value="<?= $row->kode_mk ?>"><?= $row->nama_mk ?></option>
+            <?php endforeach; ?> <?php
+        }
+    }
+
+    public function filter_dosen () 
+    {
+        $kode_mk = $this->input->post('kode_mk');
+
+        $data = $this->model_perencanaan->filter_dosen($kode_mk);
+
+        if ($kode_mk) 
+        {
+            ?> 
+                <option value="">Pilih Dosen</option>
+            <?php
+            foreach($data as $row): ?>
+                <option value="<?= $row->kode_dosen ?>"><?= $row->nama_dosen?></option>
+            <?php endforeach; ?> <?php
+        }
+    }
+
+    public function filter_kelas () 
+    {
+        $angkatan = $this->input->post('angkatan');
+
+        $data = $this->model_perencanaan->filter_kelas($angkatan);
+
+        if ($angkatan) 
+        {
+            ?> 
+                <option value="">Pilih Kelas</option>
+            <?php
+            foreach($data as $row): ?>
+                <option value="<?= $row->id ?>"><?= $row->nama_kelas?></option>
+            <?php endforeach; ?> <?php
+        }
+    }
+
+    public function update_perencanaan() {
+        $this->_rules();
+             
+        if ($this->form_validation->run() == FALSE) {
+            // $this->edit($this->input->post('id'));
+            var_dump('ERROR');
+        } else {
+            $id                 = $this->input->post('id');
+            $kode_kelompok      = $this->input->post('kode_kelompok');
+            $angkatan           = $this->input->post('angkatan');
+            $semester           = $this->input->post('semester');
+            $kode_mk            = $this->input->post('kode_mk');
+            $kode_dosen         = $this->input->post('kode_dosen');
+            $kode_kelas         = $this->input->post('kode_kelas');
+
+            $data = array (
+                'kode_kelompok'         => $kode_kelompok,
+                'angkatan_perencanaan'              => $angkatan,
+                'semester_perencanaan'  => $semester,
+                'kode_mk'               => $kode_mk,
+                'kode_dosen'            => $kode_dosen,
+                'kode_kelas'            => $kode_kelas,
+                'status_perencanaan'      => 'belum_verifikasi'
+            );
+
+            $where = array(
+                'id_perencanaan' => $id
+            );
+
+            $this->model_perencanaan->update_data($where, $data, 'tbl_perencanaan');
+            $this->session->set_flashdata('pesan','<div class="alert alert-warning alert-dismissible fade show" role="alert">
+                                                        <b>Kelas Berhasil di Ubah.</b>
+                                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                        </button>
+                                                    </div>');
+            redirect(base_url('baak/perencanaan')); 
+        }
+    }
+
+    public function verifikasi($id) 
+    {
+        $id_perencanaan = $id;  
+
+        $data = array (
+            'status_perencanaan'      => 'sudah_verifikasi'
+        );
+
+        $where = array(
+            'id_perencanaan' => $id_perencanaan
+        );
+
+        $this->model_perencanaan->update_data($where, $data, 'tbl_perencanaan');
+        $this->session->set_flashdata('pesan','<div class="alert alert-warning alert-dismissible fade show" role="alert">
+                                                    <b>Perencanaan Berhasil di Verifikasi.</b>
+                                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>');
+        redirect(base_url('baak/perencanaan'));        
+    }
+
+    public function delete($id) {
+        $where = array('id_perencanaan' => $id);
+        $this->model_perencanaan->delete_data($where, 'tbl_perencanaan');
+        $this->session->set_flashdata('pesan','<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                                        <b>Data Kelas Berhasil di Hapus.</b>
+                                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                        </button>
+                                                    </div>');
+        redirect(base_url('baak/perencanaan'));  
+    }
+
+
+    public function _rules() {
+        $this->form_validation->set_rules('kode_kelompok', 'Nama Kelompok', 'required');
+        // $this->form_validation->set_rules('angkatan', 'Angkatan', 'required');
+        // $this->form_validation->set_rules('semester', 'Semester', 'required');
+        $this->form_validation->set_rules('kode_mk', 'Mata Kuliah', 'required');
+        $this->form_validation->set_rules('kode_dosen', 'Dosen', 'required');
+        $this->form_validation->set_rules('kode_kelas', 'Kode Kelas', 'required');
+    }
+
+}
---
+++ application/views/baak/perencanaan.php
@@ -0,0 +1,78 @@
+			<!-- Main Content -->
+			<div class="main-content">
+				<section class="section">
+					<div class="section-header">
+						<h1>Perencanaan</h1>
+						<!-- <div class="section-header-breadcrumb">
+							<div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+							<div class="breadcrumb-item"><a href="#">Modules</a></div>
+							<div class="breadcrumb-item">Mata Kuliah</div>
+						</div> -->
+					</div>
+
+					<div class="section-body">						
+                        <a class="btn btn-primary mb-4" href="<?= base_url('kaprodi/perencanaan/add') ?>"> <i class="fa fa-plus fa-sm" ></i> Tambah Perencanaan </a>
+						<?= $this->session->flashdata('pesan'); ?>
+						<div class="row">
+							<div class="col-12">
+								<div class="card">
+									<div class="card-header">
+										<h4>Daftar Data Perencanaan</h4>
+										
+									</div>
+									<div class="card-body">
+										<div class="table-responsive">
+											<table class="table table-striped" id="table-1">
+												<thead>
+													<tr>
+														<th>No</th>
+														<th>Nama Kelas</th>																										
+														<th>Nama MK</th>																																																			
+														<th>Nama Dosen</th>																																																			
+														<th>Angkatan</th>																																																			
+														<th>Angkatan</th>																																																			
+														<th>Aksi</th>
+													</tr>
+												</thead>
+                                                <?php
+                                                    $no = 1; 
+													foreach($perencanaan as $_perencanaan): 
+															if ($_perencanaan->status_perencanaan == 'sudah_verifikasi'):
+                                                ?>
+													<tr>
+														<td><?= $no++ ?></td>
+														<td><?= $_perencanaan->nama_kelas ?></td>
+														<td><?= $_perencanaan->nama_mk ?></td>
+														<td><?= $_perencanaan->nama_dosen ?></td>
+														<td><?= $_perencanaan->angkatan_perencanaan ?></td>
+														<td>
+															<?php 
+																if ($_perencanaan->status_perencanaan == 'sudah_verifikasi'):
+															?>
+																	<span class="badge badge-primary">Sudah Verifikasi</span>																
+															<?php
+																else:?>
+																	<span class="badge badge-danger ">Belum Verifikasi</span>																
+															<?php	 
+																endif
+															?>
+														</td>
+														<td>
+															<?= anchor(base_url('baak/perencanaan/info/'. $_perencanaan->id_perencanaan), '<div class="btn btn-primary btn-action mr-1" data-toggle="tooltip" title="Info" href=""><i class="fas fa-search-plus"></i></div>')?>
+														</td>
+													</tr>
+												<?php
+															endif;
+													endforeach; 
+												?>
+												<tbody>                                                 
+												</tbody>
+											</table>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+			</div>
