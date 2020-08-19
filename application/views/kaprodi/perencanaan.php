117c8a3494625a805b6d0aca270da6ed7e2d8037
Winarto <winarto007zing@gmail.com>
2020-08-19 11:43:01 +0700

    Update perencanaan.php

--- application/views/kaprodi/perencanaan.php
+++ application/views/kaprodi/perencanaan.php
@@ -57,10 +57,19 @@
 															?>
 														</td>
 														<td>
-                                                            <?= anchor(base_url('kaprodi/perencanaan/verifikasi/'. $_perencanaan->id_perencanaan), '<div class="btn btn-success btn-action mr-1" data-toggle="tooltip" title="Verifikasi" href=""><i class="fas fa-check"></i></div>')?>
-															<?= anchor(base_url('kaprodi/perencanaan/info/'. $_perencanaan->id_perencanaan), '<div class="btn btn-primary btn-action mr-1" data-toggle="tooltip" title="Info" href=""><i class="fas fa-search-plus"></i></div>')?>
-															<!-- <?= anchor(base_url('kaprodi/perencanaan/edit/'. $_perencanaan->id_perencanaan), '<div class="btn btn-primary btn-action mr-1" data-toggle="tooltip" title="Edit" href=""><i class="fas fa-pencil-alt"></i></div>')?> -->
-															<?= anchor(base_url('kaprodi/perencanaan/delete/'. $_perencanaan->id_perencanaan), '<div class="btn btn-danger btn-action" data-toggle="tooltip" title="Delete"><i class="fas fa-trash"></i></div>')?>
+														<?php 
+																if ($_perencanaan->status_perencanaan == 'sudah_verifikasi'):
+															?>
+																<!-- <div class="btn btn-success btn-action mr-1 disabled" data-toggle="tooltip" title="Verifikasi" href=""><i class="fas fa-check"></i></div>		 -->
+																<?= anchor(base_url('kaprodi/perencanaan/info/'. $_perencanaan->id_perencanaan), '<div class="btn btn-primary btn-action mr-1" data-toggle="tooltip" title="Info" href=""><i class="fas fa-search-plus"></i></div>')?>
+																<?= anchor(base_url('kaprodi/perencanaan/delete/'. $_perencanaan->id_perencanaan), '<div class="btn btn-danger btn-action" data-toggle="tooltip" title="Delete"><i class="fas fa-trash"></i></div>')?>
+															<?php
+																else:?>
+																	<?= anchor(base_url('kaprodi/perencanaan/verifikasi/'. $_perencanaan->id_perencanaan), '<div class="btn btn-success btn-action mr-1 btn-block" data-toggle="tooltip" title="Verifikasi" href=""><i class="fas fa-check"></i></div>')?>																
+															<?php	 
+																endif
+															?>
+															<!-- <?= anchor(base_url('kaprodi/perencanaan/edit/'. $_perencanaan->id_perencanaan), '<div class="btn btn-primary btn-action mr-1" data-toggle="tooltip" title="Edit" href="" colspan="3"><i class="fas fa-pencil-alt"></i></div>')?> -->
 														</td>
 													</tr>
 												<?php endforeach; ?>
