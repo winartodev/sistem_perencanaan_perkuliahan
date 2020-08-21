646fff1f13cfe278ecabc7c3e2456356cb09b401
Winarto <winarto007zing@gmail.com>
2020-08-21 11:02:52 +0700

    Update Dashboard.php

--- application/controllers/Kaprodi/Dashboard.php
+++ application/controllers/Kaprodi/Dashboard.php
@@ -15,6 +15,7 @@ class Dashboard extends CI_Controller {
     }
     
     public function index() {
+        $data['pengajuan']  = $this->db->get('tbl_pengajuan')->result();
         $data['matakuliah'] = $this->model_matakuliah->count_data();
         $data['dosen']      = $this->model_dosen->count_data();
         $data['kelas']      = $this->model_kelas->count_data();
@@ -22,5 +23,18 @@ class Dashboard extends CI_Controller {
         $this->load->view('templates/kaprodi/sidebar');
         $this->load->view('kaprodi/dashboard', $data);
         $this->load->view('templates/kaprodi/footer');
+    }
+
+    public function delete_pengajuan($id) 
+    {
+        $where = array('id_pengajuan' => $id);
+        $this->model_perencanaan->delete_data($where, 'tbl_pengajuan');
+        $this->session->set_flashdata('pesan','<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                                        <b>Data Dosen Berhasil di Hapus.</b>
+                                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                        </button>
+                                                    </div>');
+        redirect(base_url('kaprodi/dashboard')); 
     }
 }
