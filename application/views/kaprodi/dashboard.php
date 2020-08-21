def33c7cb735b26d4d344a292714b813040333f2
Winarto <winarto007zing@gmail.com>
2020-08-21 11:03:17 +0700

    Update dashboard.php

--- application/views/kaprodi/dashboard.php
+++ application/views/kaprodi/dashboard.php
@@ -35,7 +35,7 @@
       					</div>
       				</div>
       			</div>
-				  <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+      			<div class="col-lg-3 col-md-6 col-sm-6 col-12">
       				<div class="card card-statistic-1">
       					<div class="card-icon bg-warning">
       						<i class="fa fa-building fa-2x" style="color:#ffffff;"></i>
@@ -52,4 +52,42 @@
       			</div>
       		</div>
       	</section>
+
+      	<div class="row">
+      		<div class="col-12">
+      			<div class="card">
+      				<div class="card-header">
+      					<h4>Daftar Pengajuan Pembukaan Matakuliah</h4>
+      				</div>
+      				<div class="card-body">
+      					<div class="table-responsive">
+      						<table class="table table-striped" id="table-1">
+      							<thead>
+      								<tr>
+      									<th>NPM</th>
+      									<th>Nama Mahasiswa</th>
+      									<th>Email</th>
+      									<!-- <th>No Telp</th> -->
+      									<th>Aksi</th>
+      								</tr>
+      							</thead>
+      							<tbody>
+								  <?php foreach($pengajuan as $_pengajuan): ?>
+      							<tr>
+      								<td><?= $_pengajuan->npm; ?></td>
+      								<td><?= $_pengajuan->nama_mahasiswa; ?></td>
+      								<td><?= $_pengajuan->konten; ?></td>
+      								<!-- <td><?= $_pengajuan->no_telp; ?></td> -->
+      								<td>
+      									<?= anchor(base_url('kaprodi/dashboard/delete_pengajuan/'. $_pengajuan->id_pengajuan), '<div class="btn btn-danger btn-action" data-toggle="tooltip" title="Delete"><i class="fas fa-trash"></i></div>')?>
+      								</td>
+      							</tr>
+      							<?php endforeach; ?>
+      							</tbody>
+      						</table>
+      					</div>
+      				</div>
+      			</div>
+      		</div>
+      	</div>
       </div>
