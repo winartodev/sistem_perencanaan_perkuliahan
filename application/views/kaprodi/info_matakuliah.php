d120db780af28ba0cbe7fd6cfd387011f215f832
Winarto <winarto007zing@gmail.com>
2020-08-21 15:47:42 +0700

    Update info_matakuliah.php

--- application/views/kaprodi/info_matakuliah.php
+++ application/views/kaprodi/info_matakuliah.php
@@ -2,9 +2,7 @@
 			<div class="main-content">
 				<section class="section">
 					<div class="section-header">
-						<?php foreach($matakuliah as $_matakuliah):?>
-						<h1><?= $_matakuliah->nama_kelas, ' (',$_matakuliah->nama_mk,')' ?></h1>
-						<?php endforeach; ?>
+						<h1>Matakuliah</h1>
 						<!-- <div class="section-header-breadcrumb">
 							<div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
 							<div class="breadcrumb-item"><a href="#">Modules</a></div>
@@ -15,10 +13,29 @@
 					<div class="section-body">
 						<div class="card">
 							<div class="card-body">
+							<div class="col-md-6">
+								<table class="table table-striped">
+									<?php foreach($matakuliah as $_matakuliah):?>
+									<tr>
+										<th>Kode MK</th>
+										<th><?= $_matakuliah->kode_mk; ?></th>
+									</tr>
+									<tr>
+										<th>Nama Matakuliah</th>
+										<th><?= $_matakuliah->nama_mk; ?></th>
+									</tr>
+									<tr>
+										<th>Jumlah Mahasiswa </th>
+										<th><?= $this->model_matakuliah->jumlah_mahasiswa($_matakuliah->kode_mk); ?></th>
+									</tr>
+									<?php endforeach; ?>
+								</table>
+							</div>
+								
 								<div class="row">
 									<div class="col-md-6">
-										<h6>Data Mahasiswa</h6>
-										<div class="table-responsive">
+									<h4 align="center">Data Mahasiswa</h4>
+										<div class="table-responsive mt-4">
 											<table class="table table-striped" id='table-1'>
 												<thead>
 													<tr>
