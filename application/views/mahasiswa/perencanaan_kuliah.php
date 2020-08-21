790f08e811f75bc95a10c93e4ad793085bd8a016
Winarto <winarto007zing@gmail.com>
2020-08-21 11:03:31 +0700

    Update perencanaan_kuliah.php

--- application/views/mahasiswa/perencanaan_kuliah.php
+++ application/views/mahasiswa/perencanaan_kuliah.php
@@ -2,7 +2,7 @@
 			<div class="main-content">
 				<section class="section">
 					<div class="section-header">
-						<h1>Perencanaan Perkuliahan</h1>
+						<h1>Pra Perencanaan Perkuliahan</h1>
 						<!-- <div class="section-header-breadcrumb">
 							<div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
 							<div class="breadcrumb-item"><a href="#">Modules</a></div>
@@ -17,20 +17,21 @@
 							<div class="col-12">
 								<div class="card">
 									<div class="card-header">
-										<h4>Daftar Kelas</h4>
+										<h4>Daftar Pra Perencanaan</h4>
 									</div>
 									<div class="card-body">
 										<div class="table-responsive">
 											<table class="table table-striped" id="table-1">
 												<thead>
 													<tr>
+														<th>ID Perencanaan</th>
 														<th>Kelas</th>
 														<th>Kode MK</th>
                                                         <th>Nama Mk</th>
-                                                        <!-- <th>Nama Dosen</th> -->
 														<th>SKS Teori</th>																																																																																																																																																																																																																							
 														<th>SKS Praktek</th>																																																																																																																																																																																																																							
-														<th>Total SKS</th>																																																																																																																																																																																																																							
+														<th>Total SKS</th>	
+														<th>Jumlah Mahasiswa</th>	
 														<th>Aksi</th>
 													</tr>
 												</thead>
@@ -42,13 +43,14 @@
 																if ($kls->status_perencanaan == 'sudah_verifikasi'):
 													?>
 													<tr>
+														<td><?= $kls->id_perencanaan; ?></td>
 														<td><?= $kls->nama_kelas; ?></td>
 														<td><?= $kls->kode_mk; ?></td>
 														<td><?= $kls->nama_mk; ?></td>
-														<!-- <td><?= $kls->nama_dosen; ?></td> -->
 														<td><?= $kls->sks_teori; ?></td>
 														<td><?= $kls->sks_praktek; ?></td>
 														<td><?= $kls->total_sks; ?></td>
+														<td> <?= $this->model_perencanaan->jumlah_mahasiswa($kls->id_perencanaan) ?> / 2</td>
 														<td>
 															<?= anchor(base_url('mahasiswa/perencanaan/daftar/'. $kls->id_perencanaan), '<div class="btn btn-warning btn-action mr-1 text-bold" href="">Daftar</div>')?>
 														</td>
