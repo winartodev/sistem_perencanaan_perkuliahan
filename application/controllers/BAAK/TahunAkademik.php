b33a23816978128bb609e740889c8c4b5de47568
Winarto <winarto007zing@gmail.com>
2020-08-21 11:02:49 +0700

    Update TahunAkademik.php

--- application/controllers/BAAK/TahunAkademik.php
+++ application/controllers/BAAK/TahunAkademik.php
@@ -25,6 +25,7 @@ class TahunAkademik extends CI_Controller {
     public function check_tahun_akademik() 
     {
         $tahun_akademik = $this->model_tahun_akademik->count_data();
+        $data = $this->model_tahun_akademik->read_data();
         if ($tahun_akademik == 0) 
         {
             $this->session->set_flashdata('check_tahun_akademik','<div class="alert alert-warning alert-dismissible fade show" role="alert">
@@ -42,8 +43,8 @@ class TahunAkademik extends CI_Controller {
                                                 <br><br>
                                                 <a class="btn btn-info btn-sm mr-3" href='.base_url('baak/penginputan/input_tanggal_perencanaan').'> 
                                                 Buat Tanggal Perencanaan
-                                                </a>  
-                                                <a class="btn btn-danger btn-sm" href='.base_url('baak/tahunakademik/buat_tahun_akademik').'> 
+                                                </a> 
+                                                <a class="btn btn-danger btn-sm" href='.base_url('baak/tahunakademik/ubah_tahun_akademik/'. 6).'> 
                                                 Ubah Tahun Akademik
                                                 </a> 
                                             </div>');
@@ -58,6 +59,16 @@ class TahunAkademik extends CI_Controller {
         $this->load->view('templates/baak/footer');
     }
 
+    public function ubah_tahun_akademik($id) 
+    {
+        $where = array('id_ta' => $id);
+        $data['tahun_akademik']    = $this->model_tahun_akademik->form_edit($where, 'tbl_tahun_akademik');
+        $this->load->view('templates/baak/header');
+        $this->load->view('templates/baak/sidebar');
+        $this->load->view('baak/ubah_tahun_akademik', $data);
+        $this->load->view('templates/baak/footer');
+    }
+
     public function insert_tahun_akademik() 
     {
         $this->_rules();
@@ -67,7 +78,6 @@ class TahunAkademik extends CI_Controller {
         } else {
             $tahun_akademik     = $this->input->post('tahun_akademik');
             $semester   = $this->input->post('semester');
-            $konten    = $this->input->post('konten');
 
             $data = array (
                 'tahun_akademik'    => $tahun_akademik,
@@ -78,6 +88,38 @@ class TahunAkademik extends CI_Controller {
             
             $this->session->set_flashdata('pesan','<div class="alert alert-info alert-dismissible fade show" role="alert">
                                                         <b>Tahun Akademik berhasil di Buat.</b>
+                                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                        </button>
+                                                    </div>');
+            redirect(base_url('baak/tahunakademik')); 
+        }
+    }
+
+    public function update_tahun_akademik() 
+    {
+        $this->_rules();
+             
+        if ($this->form_validation->run() == FALSE) {
+            $this->ubah_tahun_akademik();
+        } else {
+            $id_ta     = $this->input->post('id_ta');
+            $tahun_akademik     = $this->input->post('tahun_akademik');
+            $semester   = $this->input->post('semester');
+
+            $data = array (
+                'tahun_akademik'    => $tahun_akademik,
+                'semester'  => $semester,
+            );
+
+            $where = array (
+                'id_ta' => $id_ta
+            );
+
+            $this->model_tahun_akademik->update_data( $where, $data,'tbl_tahun_akademik');
+            
+            $this->session->set_flashdata('pesan','<div class="alert alert-info alert-dismissible fade show" role="alert">
+                                                        <b>Tahun Akademik berhasil di Ubah.</b>
                                                         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                                         <span aria-hidden="true">&times;</span>
                                                         </button>
