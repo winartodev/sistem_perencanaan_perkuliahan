90902b09a62dacd5e6aecd033d540a2ee829653e
Winarto <winarto007zing@gmail.com>
2020-09-07 21:21:25 +0700

    Update perencanaan_kuliah.php

--- application/views/mahasiswa/perencanaan_kuliah.php
+++ application/views/mahasiswa/perencanaan_kuliah.php
@@ -2,7 +2,7 @@
 			<div class="main-content">
 				<section class="section">
 					<div class="section-header">
-						<h1>Pra Perencanaan Perkuliahan</h1>
+						<h1>Perencanaan Perkuliahan</h1>
 						<!-- <div class="section-header-breadcrumb">
 							<div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
 							<div class="breadcrumb-item"><a href="#">Modules</a></div>
@@ -17,7 +17,7 @@
 							<div class="col-12">
 								<div class="card">
 									<div class="card-header">
-										<h4>Daftar Pra Perencanaan</h4>
+										<h4>Daftar Perencanaan</h4>
 									</div>
 									<div class="card-body">
 										<div class="table-responsive">
